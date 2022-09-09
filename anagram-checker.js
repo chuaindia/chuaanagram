9bd55678063e642f67c4aaafd8856f0c00d226ad
Tanusri Ghosh <tghosh25@gmail.com>
2022-09-09 19:55:09 +0530

    Update anagram-checker.js

--- anagram-checker.js
+++ anagram-checker.js
@@ -1,26 +1,30 @@
-module.exports = function (str1, str2) {      
-let x1 = str1.toLowerCase();
-let y1 = x1.replace(/[^a-zA-Z0-9]/g, '');
-let x2 = str2.toLowerCase();
-let y2 = x2.replace(/[^a-zA-Z0-9]/g, '');
-let l1 = y1.length;
-let l2 = y2.length;     
-if(l1 != l2)
-    return false;     
-let arr1 = [];      
-let arr2 = [];     
-for (i=0; i<l1; i++) {
-    arr1.push(str1.charAt([i]));
-}     
-for (i=0; i<l2; i++) {
-    arr2.push(str2.charAt([i]));
-}    
-arr1.sort();      
-arr2.sort();    
-if (arr1 === arr2){
-    return true;       
-}  
-else
-{   return true;     
-}  
+module.exports = function (str1, str2) {
+  // write your code here
+let by = 0;
+let x = str1.toLowerCase()
+    y = x.replace(/[^a-zA-Z0-9]/g, '')
+    ascii1 = [];
+    for (i=0; i<y.length; i++) {
+      ascii1.push(y.charCodeAt([i]))
+    }
+let sorted1 = ascii1.sort()
+let a = str2.toLowerCase()
+    b = a.replace(/[^a-zA-Z0-9]/g, '')
+    ascii2 = [];
+    for (i=0; i<y.length; i++) {
+      ascii2.push(b.charCodeAt([i]))
+    }
+let sorted2 = ascii2.sort()
+for (i=0; i<y.length; i++) {
+  if (ascii1[i] != ascii2[i]) {
+    by--
+  } else {
+    by++
+  }
+}
+if ((by === y.length) && (by === b.length)) {
+  return true
+} else {
+  return false
+}
 }
